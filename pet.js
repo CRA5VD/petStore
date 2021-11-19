65a03bef481de08c91e201fd4628fbc9b9f473c2
Connie <constancecrawford87@gmail.com>
2021-11-19 10:27:50 -0500

    petstore

--- pet.js
+++ pet.js
@@ -1,20 +1,38 @@
 var shop = [
     {
-      title: 'Ella',
-      image: './img/flouffy-unsplash.jpg',
-      price: '$850-$1000',
+      title: 'Indigo',
+      image: './img/indigo.jpg',
+      price: '$25',
       description: '',
     },
     {
-      title: 'Otis',
-      image: './img/Otis-unsplash.jpg',
-      price: '$850-$1000',
+      title: 'Orange',
+      image: './img/satsuma.jpg',
+      price: '$25',
       description: '',
     },
     {
-      title: 'Simone',
-      image: './img/smallDog-unsplash.jpg',
-      price: '$850-$1000',
+      title: 'Navy',
+      image: './img/navy.jpg',
+      price: '$25',
+      description: '',
+    },
+    {
+      title: 'Rainbow',
+      image: './img/rainbow.jpg',
+      price: '$25',
+      description: '',
+    },
+    {
+      title: 'Chevron',
+      image: './img/tfd-x.jpg',
+      price: '$25',
+      description: '',
+    },
+    {
+      title: 'Peaches',
+      image: './img/peaches.jpg',
+      price: '$25',
       description: '',
     }
     
@@ -23,18 +41,20 @@ var shop = [
     ]
     
     var postHTML = " "
-  
-  
 
-  
-  
-  
-    for (var i=0; i < shop.length; i++){
-        var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
-        var image = '<img id="sameSize" src="' + shop[i].image + '"/'
-        var price = '<p> $' + shop[i].price + '</p></span>'
-        var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
-        var concatThis = heading + image + price + description;
-        postHTML = postHTML + concatThis
+    for (var i=0; i < shop.length; i++) {
+        // var heading = '<div class="product ' + '"><h5>' + shop[i].title + '</h5>'
+        // var image = '<div class="proPrice"><img id="sameSize" src="' + shop[i].image + '"/'
+        // var price = '<p> ' + shop[i].price + '</p></div>'
+        // var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-light"> add to cart</button></div></div>'
+        // var concatThis = heading + image + price + description;
+        // postHTML = postHTML + concatThis
+
+        let heading = '<div class="product col-sm-6 col-md-4"><h5>' + shop[i].title + '</h5>';
+        let image = '<img class="img-fluid mb-4" src="' + shop[i].image + '"/';
+        let price = '<div><p>' + '<span class="price"> $' + shop[i].price + '</span>';
+        let description = shop[i].description + '</p><a class="cart" href="#"><i class="fas fa-cart-plus"></i></a> <a class="view" href="#" target="_blank"> View Item</a></div></div>';
+        let concatThis = heading + image + price + description;
+        postHTML = postHTML + concatThis;
     }
     document.getElementById('market').innerHTML = postHTML
